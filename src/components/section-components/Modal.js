8f16b7a45e727ed61d1b7518a7850e47cdb612cc
mehdi modarres <mr.modarres1994@gmail.com>
2023-01-05 21:28:38 +0330

    + add file and json for deploy in netlify

--- src/components/section-components/Modal.js
+++ src/components/section-components/Modal.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BiCheck, BiX } from "react-icons/bi";
+import { BiX } from "react-icons/bi";
 import ModalInfo from "../section/ModalInfo";
 
 function Modal({ show, setShow, modalData, setModalData }) {
-  console.log(modalData);
   const closeHandle = () => {
     setShow(false);
     setModalData(null);
