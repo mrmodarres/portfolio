faa9adcdb7b87a10f1052de468f986dff151d52e
mehdi modarres <mr.modarres1994@gmail.com>
2023-02-28 00:05:14 +0330

    +fix show page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Layout from "./components/Layout";
 import Main from "./components/Main";
 
 import { I18Provider, LOCALES } from "./i18n";
+import { FormattedMessage } from "react-intl";
 function App() {
   return (
-    <I18Provider locale={LOCALES.english}>
-      <Layout>
-        <Main />
-      </Layout>
-    </I18Provider>
+    // <I18Provider locale={LOCALES.english}>
+    <Layout>
+      <Main />
+    </Layout>
+    // </I18Provider>
   );
 }
 
